d6f2a76b44fd68ec79856fd7a10499eb4c907697
Jacob Yang <yajacob@gmail.com>
2017-10-20 01:46:05 -0700

    Apply new html & url refactoring

--- src/main/java/net/inzoe/web/UserController.java
+++ src/main/java/net/inzoe/web/UserController.java
@@ -8,28 +8,35 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.inzoe.domain.User;
 import net.inzoe.domain.UserRepository;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 	private List <User> users = new ArrayList<User>();
 	
 	@Autowired
 	private UserRepository userRepository;
-	
-	@PostMapping("/user/create")
+
+	@GetMapping("/form")
+	public String userForm() {
+		return "/user/form";
+	}
+
+	@PostMapping("/create")
 	public String createUser(User user) {
 		//users.add(user);
 		System.out.println("user:"+user.toString());
 		userRepository.save(user);
-		return "redirect:list";
+		return "redirect:/users/list";
 	}
 	
 	@GetMapping("/list")
 	public String userList(Model model) {
 		model.addAttribute("users", userRepository.findAll());
-		return "/list";
+		return "/user/list";
 	}
 }

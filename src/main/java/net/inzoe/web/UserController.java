8f4d15f014c4ba8f2c12f0b1968bdc614cbcc48d
Jacob Yang <yajacob@gmail.com>
2017-10-20 23:28:35 -0700

    login process

--- src/main/java/net/inzoe/web/UserController.java
+++ src/main/java/net/inzoe/web/UserController.java
@@ -1,5 +1,7 @@
 package net.inzoe.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,6 +25,27 @@ public class UserController {
 		return "/users/form";
 	}
 
+	@GetMapping("/login")
+	public String login() {
+		return "/users/login";
+	}
+	
+	@PostMapping("/loginProc")
+	public String loginProc(String userId, String password, HttpSession session) {
+		User user = userRepository.findByUserId(userId);
+		if (user == null) {
+			return "redirect:/users/login";
+		}
+		
+		if (!password.equals(user.getPassword())) {
+			return "redirect:/users/login";
+		}
+		
+		session.setAttribute("user", user);
+		
+		return "redirect:/";
+	}
+	
 	@PostMapping("/create")
 	public String create(User user) {
 		userRepository.save(user);
